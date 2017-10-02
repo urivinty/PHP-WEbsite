49777c8cbc74b0e1c0e0cfe6798d209beaac85f1
urivinty <31045617+urivinty@users.noreply.github.com>
2017-10-02 05:53:40 -0800

    Initial commit

---
+++ sample php.php
@@ -0,0 +1,24 @@
+<?php
+
+print "<html>\n";
+print "\n";
+print "<head>\n";
+print "<title> My new website </title>\n";
+print "<link href=\"style.css\" type=\"text/css\" rel=\"stylesheet\">\n";
+print "</head>\n";
+print "\n";
+print "\n";
+print "<body>\n";
+print "<div id=\"main\">\n";
+print " Jagadeesh chandra Urivinty. Gouthami Manganoor. Moukthika Gouthami.\n";
+print "\n";
+print " Austin Tx.\n";
+print "\n";
+print " Buddi 0910.\n";
+print " \n";
+print " dyuthi. chitti\n";
+print "\n";
+print "</body>\n";
+print "</html>";
+
+?>
